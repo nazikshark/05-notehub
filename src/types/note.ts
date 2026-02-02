724dd33ff0d1b09174b71c51ac86dc9bc47a698c
nazikshark <nazarpro783@gmail.com>
2026-02-02 16:09:27 +0100

    one

--- src/types/note.ts
+++ src/types/note.ts
@@ -1,8 +1,10 @@
-export type NoteTag = 'Todo' | 'Work' | 'Personal' | 'Meeting' | 'Shopping';
+export type NoteTag = 'Work' | 'Personal' | 'Home' | 'Important' | 'Todo';
 
 export interface Note {
   id: string;
   title: string;
   content: string;
   tag: NoteTag;
+  createdAt: string; // Додано
+  updatedAt: string; // Додано
 }
