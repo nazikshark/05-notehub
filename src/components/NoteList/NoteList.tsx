03c3a1b5fb52cf5ac862485ca19466f84f1433fb
nazikshark <nazarpro783@gmail.com>
2026-02-02 16:38:53 +0100

    gfhdj

--- src/components/NoteList/NoteList.tsx
+++ src/components/NoteList/NoteList.tsx
@@ -1,12 +1,28 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { deleteNote } from '../../services/noteService';
 import type { Note } from '../../types/note';
 import css from './NoteList.module.css';
 
 interface NoteListProps {
   notes: Note[];
-  onDelete: (id: string) => void; 
 }
 
-const NoteList = ({ notes, onDelete }: NoteListProps) => {
+const NoteList = ({ notes }: NoteListProps) => {
+  const queryClient = useQueryClient();
+
+  // Реалізація мутації для видалення всередині компонента
+  const { mutate: deleteMutate } = useMutation({
+    mutationFn: (id: string) => deleteNote(id),
+    onSuccess: () => {
+      // Автоматичне оновлення списку після успішного видалення
+      queryClient.invalidateQueries({ queryKey: ['notes'] });
+    },
+    onError: (error) => {
+      console.error('Помилка при видаленні:', error);
+      alert('Не вдалося видалити нотатку');
+    },
+  });
+
   return (
     <ul className={css.list}>
       {notes.map((note) => (
@@ -15,7 +31,10 @@ const NoteList = ({ notes, onDelete }: NoteListProps) => {
           <p className={css.content}>{note.content}</p>
           <div className={css.footer}>
             <span className={css.tag}>{note.tag}</span>
-            <button className={css.button} onClick={() => onDelete(note.id)}>
+            <button 
+              className={css.button} 
+              onClick={() => deleteMutate(note.id)}
+            >
               Delete
             </button>
           </div>
